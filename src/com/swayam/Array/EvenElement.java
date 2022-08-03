6c5265efe3c874aa8fcabfab5277b4e14c3767f5
swayampandey <swayam265@gmail.com>
2022-08-03 10:48:04 +0530

    [Swayam] : added basic array programs

--- src/com/swayam/Array/EvenElement.java
+++ src/com/swayam/Array/EvenElement.java
@@ -1,30 +1,29 @@
 package com.swayam.Array;
 import java.util.Scanner;
 
-public class ArrayElementMin {
+public class EvenElement {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the size of array");
 		int size = sc.nextInt();
-		int[] a = new int[size];
+		int [] a = new int[size];
 		System.out.println("Enter the elements of array");
-		for(int i = 0; i < a.length; i++) {
+		for(int  i = 0; i < a.length; i++) {
 			a[i] = sc.nextInt();
 		}
-		System.out.println("Elements of array are");
-		for(int i = 0; i < a.length; i++) {
+		System.out.println("Elements of array are : ");
+		for(int  i = 0; i < a.length; i++) {
 			System.out.print(" "+a[i]+" ");
 		}
-		int min = a[0];
-		for(int i = 0; i < a.length; i++) {
-			if(a[i] < min ) {
-				min = a[i];
-			}
-		}
 		System.out.println();
-		System.out.println("Minimum element of array is " +min);
-		sc.close();
+		System.out.println("Even elements of array are : ");
+		for(int  i = 0; i < a.length; i++) {
+			if(a[i] % 2 == 0) {
+				System.out.print(" "+a[i]+" ");
+			}
 	}
+		sc.close();
+		}
 
 }
--- src/com/swayam/Array/OddElement.java
+++ src/com/swayam/Array/OddElement.java
@@ -1,25 +1,28 @@
 package com.swayam.Array;
 import java.util.Scanner;
 
-public class SumOfElements {
-static int sum;
+public class OddElement {
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the size of element");
 		int size = sc.nextInt();
-		int [] a = new int[size];
+		int[] a = new int[size];
 		System.out.println("Enter the elements of array");
-		for(int i = 0; i<a.length; i++) {
+		for (int i = 0; i < a.length; i++) {
 			a[i] = sc.nextInt();
 		}
-		System.out.println("Elements of array are");
-		for(int i = 0; i<a.length; i++) {
+		System.out.println("Elements of array are : ");
+		for (int i = 0; i < a.length; i++) {
 			System.out.print(" "+a[i]+" ");
-			sum = sum + a[i];
 		}
 		System.out.println();
-		System.out.println("Sum of elements of array is : ");
-		System.out.println(+sum);
+		System.out.println("Odd elements of array are : ");
+		for (int i = 0; i < a.length; i++) {
+			if(a[i] %2!= 0) {
+				System.out.print(" "+a[i]+" ");
+				}
+			}
 		sc.close();
 	}
 
